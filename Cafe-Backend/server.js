9951450e37b39818973a3d6c4abe62e307214838
Sagar Chowdary <sagarchowdary99511@gmail.com>
2026-06-03 10:44:03 +0530

    For Elastic Beanstalk

--- Cafe-Backend/server.js
+++ Cafe-Backend/server.js
@@ -12,18 +12,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Home Route
 app.get("/", (req, res) => {
-  res.send("Cafe Backend Running");
+  res.send("Cafe Backend Running Successfully");
 });
 
-// Menu Route
 app.use("/menu", menuRoutes);
-
-// Customer Route
 app.use("/customers", customerRoutes);
-
-// Order Route
 app.use("/orders", orderRoutes);
 
 const PORT = process.env.PORT || 3000;
